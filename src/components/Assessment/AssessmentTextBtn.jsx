cf989f0466f568cb06287561e5ec92e43c71fc6b
Omprakash Prajapati <oprajapati249@gmail.com>
2023-08-07 16:04:10 +0530

    updated assessmemt slide navigation

--- src/components/Assessment/AssessmentTextBtn.jsx
+++ src/components/Assessment/AssessmentTextBtn.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function AssessmentTextBtn(props) {
+  const [selectOption, setselectOption] = useState(false);
+  function name(params) {}
+
   return (
-    <div className="bg-gradient-to-r from-secondary to-tertiary rounded-lg flex items-stretch p-[3px] box-border group">
+    <div
+      onClick={selectOption}
+      className="bg-gradient-to-r from-secondary to-tertiary rounded-lg flex items-stretch p-[3px] box-border group"
+    >
       <button className="flex items-center px-5 py-2.5  hover:text-white w-full justify-center font-semibold bg-white rounded-md hover:bg-primary-300">
         {props.name}
       </button>
