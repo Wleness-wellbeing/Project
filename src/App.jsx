d946269ca383ff5aec97bb6abe21720264b8d89b
Omprakash Prajapati <oprajapati249@gmail.com>
2023-08-17 20:42:48 +0530

    updated admin pages

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ import Faqs from "./pages/Faqs";
 import Community from "./pages/Community";
 import IssueSubPageLayout from "./pages/Issues/IssueSubPageLayout";
 import Admin from "./pages/Admin";
-// import AdminSideBar from "./components/admin/AdminSideBar";
+import AdminSideBar from "./components/admin/AdminSideBar";
 import IssueCategory from "./pages/Admin/IssueCategory";
 import AdminIssues from "./pages/Admin/AdminIssues";
 import AdminQuestions from "./pages/Admin/AdminQuestions";
@@ -437,11 +437,11 @@ function SignupLayout({ children }) {
 function AdminLayout({ children }) {
   // This layout is specifically for /admin page
   return (
-    <section class="container mx-auto grid justify-center py-12">
-      <h1 class="col-span-2 text-3xl font-semibold pb-8 ml-[200px]">
+    <section className="container mx-auto grid justify-center py-12">
+      <h1 className="col-span-2 text-3xl font-semibold pb-8 ml-[200px]">
         Admin Page
       </h1>
-      {/* <AdminSideBar /> */}
+      <AdminSideBar />
       {children}
     </section>
   );
---
+++ src/components/admin/AdminSideBar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { adminSidebar } from "../../data/navigation";
+
+export default function AdminSideBar() {
+  return (
+    <aside className="w-[200px] p-4 xl:p-6">
+      <ul>
+        {adminSidebar.map((value, i) => {
+          return (
+            <Link
+              key={i}
+              to={value[1]}
+              className="my-2 text-right block admin-link-hover font-medium"
+            >
+              {value[0]}
+            </Link>
+          );
+        })}
+      </ul>
+    </aside>
+  );
+}
