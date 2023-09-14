c324bfc27d04a6919432b1d064feaf997c283158
Omprakash Prajapati <oprajapati249@gmail.com>
2023-09-14 17:31:16 +0530

    updated test api uri

--- src/data/api.js
+++ src/data/api.js
@@ -1,5 +1,6 @@
 // const URI = 'https://wleness.com'
-const URI = "http://127.0.0.1:5000/";
+const URI = "http://45.79.126.107/";
+// const URI = "http://127.0.0.1:5000/";
 
 // API Endpoints
 export const REQUEST_CALLBACK_URI = URI + "request-callback";
