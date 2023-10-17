58fe9164d09a5b75ca306f7f4dad1aa8493c93f4
prabhat20012001 <prabhatkumarmnnit@gmail.com>
2023-10-17 22:35:59 +0530

    header  and activities image change

--- src/components/Cards/ServicesCard.jsx
+++ src/components/Cards/ServicesCard.jsx
@@ -4,15 +4,10 @@ import { Link } from "react-router-dom";
 export default function ServicesCard(props) {
   return (
     <>
-      <div className="rounded-2xl bg-gradient-to-br from-secondary to-tertiary p-1 transition-all lg:hover:scale-105">
+      <div className="rounded-2xl ">
         <Link to={props.data.slug}>
-          <figure className="hover:bg-yellow-primary relative flex h-full flex-col justify-between rounded-xl bg-white p-3 transition-all hover:shadow-xl hover:shadow-secondary/30 lg:p-5">
-            <img
-              src={props.data.image}
-              alt={props.data.title}
-              className="absolute -top-[15%] left-1/2 mx-auto w-16 -translate-x-1/2 translate-y-1/4 drop-shadow-lg lg:-top-1/4 lg:w-24"
-            />
-            <figcaption className="pb-2 pt-8 lg:pb-5 lg:pt-10">
+          <img src={props.data.image} className="" />
+          {/* <figcaption className="pb-2 pt-8 lg:pb-5 lg:pt-10">
               <h2 className="mb-2 text-center text-sm font-semibold lg:text-lg xl:text-xl">
                 {props.data.title}
               </h2>
@@ -22,8 +17,7 @@ export default function ServicesCard(props) {
             </figcaption>
             <button className="mx-auto block w-fit rounded-full bg-gradient-to-tr from-secondary to-tertiary px-5 py-2.5 text-xs font-semibold text-white transition-all hover:shadow-lg lg:text-base">
               Explore More
-            </button>
-          </figure>
+            </button> */}
         </Link>
       </div>
     </>
