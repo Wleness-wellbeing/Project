a56b96d54b5996f619b1b2aff5ff7541f380ad36
Omprakash Prajapati <oprajapati249@gmail.com>
2023-10-31 13:00:06 +0530

    updated audio play and pause

--- src/components/Cards/MusicTracksCard.jsx
+++ src/components/Cards/MusicTracksCard.jsx
@@ -1,11 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef } from "react";
 
 export default function MusicTracksCard(props) {
-  const [play, setPlay] = useState(false);
+  const progressRef = useRef(null);
+  const currentTimeRef = useRef(null);
+  const animationFrameRef = useRef(null);
 
-  const togglePlay = () => {
-    setPlay(!play);
+  const updateProgressBar = () => {
+    if (progressRef.current) {
+      const currentTime = props.audio.currentTime;
+      const duration = props.audio.duration;
+      const progress = (currentTime / duration) * 100;
+      progressRef.current.style.width = progress + "%";
+      currentTimeRef.current.innerText = Math.round(currentTime);
+    }
+    if (props.isPlaying) {
+      animationFrameRef.current = requestAnimationFrame(updateProgressBar);
+    }
   };
+
+  useEffect(() => {
+    if (props.isPlaying) {
+      props.audio.currentTime = 0; // Reset to the beginning
+      props.audio.play();
+      updateProgressBar();
+    } else {
+      props.audio.pause();
+      cancelAnimationFrame(animationFrameRef.current);
+    }
+  }, [props.isPlaying, props.audio]);
 
   return (
     <div className="group relative cursor-pointer">
@@ -22,21 +44,29 @@ export default function MusicTracksCard(props) {
           </div>
         </div>
         <figcaption className="flex flex-col items-center justify-between gap-y-2 px-2 py-5 text-center">
-          {play && (
+          {props.isPlaying && (
             <div className="w-full px-2 !text-left">
               <div className="bg-primary-10">
-                <span className="block h-1 w-8 rounded-full bg-primary-300"></span>
+                <span
+                  ref={progressRef}
+                  style={{ width: "0%" }}
+                  className="block h-1 rounded-full bg-primary-300"
+                ></span>
               </div>
               <div className="flex justify-between text-xs">
-                <small>1:14</small>
-                <small>4:58</small>
+                <small ref={currentTimeRef}></small>
+                <small>
+                  {Math.round(
+                    (props.audio.duration / 60 + Number.EPSILON) * 100,
+                  ) / 100}
+                </small>
               </div>
             </div>
           )}
 
           <p className="text-center font-semibold">{props.data.desc}</p>
-          <button className="btn-one !py-2" onClick={() => togglePlay()}>
-            {play ? "Pause" : "Play Now"}
+          <button className="btn-one !py-2" onClick={() => props.togglePlay()}>
+            {props.isPlaying ? "Pause" : "Play Now"}
           </button>
         </figcaption>
       </figure>
