26b150905e4ded1be27d8978c86b05954b032979
Omprakash Prajapati <oprajapati249@gmail.com>
2024-01-31 12:54:36 +0530

    added expert coupon

--- src/pages/Experts/ExpertsDetails.jsx
+++ src/pages/Experts/ExpertsDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import DoctorsCard from "../../components/DoctorsCard";
 import HappyClient from "../../components/HappyClient";
 import { expertsClients } from "../../data/clients";
