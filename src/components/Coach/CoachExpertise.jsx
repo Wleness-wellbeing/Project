1b471e798ee4895d5be70416f0dd85879984f349
Omprakash Prajapati <oprajapati249@gmail.com>
2024-02-09 12:09:20 +0530

    responsive assessment result page

--- src/components/Coach/CoachExpertise.jsx
+++ src/components/Coach/CoachExpertise.jsx
@@ -5,8 +5,8 @@ export default function CoachExpertise({ heading, image, lists }) {
   return (
     <section className="mx-auto flex flex-col-reverse border-4 border-secondary/10 md:flex-row">
       <div className="w-full bg-secondary/10 md:w-3/4">
-        <div className="my-16 w-full text-center md:ml-60 md:mr-0 md:w-2/4 md:text-left">
-          <div className="text-center">
+        <div className="my-16 ml-4 w-full sm:ml-20 md:mr-0 md:text-left lg:ml-32 xl:ml-44">
+          <div>
             <h2 className="subheading ">{heading}</h2>
           </div>
 
@@ -26,7 +26,7 @@ export default function CoachExpertise({ heading, image, lists }) {
         </div>
       </div>
 
-      <div className="mx-auto my-auto transform md:mt-0 md:translate-x-0 lg:my-auto lg:-translate-x-60 lg:text-center">
+      <div className="m-auto md:-translate-x-20 lg:-translate-x-32 lg:text-center xl:-translate-x-44">
         <img
           src={image} // Replace with your image URL
           alt="Description of the image"
