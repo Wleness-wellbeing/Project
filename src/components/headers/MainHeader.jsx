969809239e83142bf01cb240b7b5d1ca35fe15f9
Omprakash Prajapati <oprajapati249@gmail.com>
2024-02-20 16:38:36 +0530

    updated

--- src/components/headers/MainHeader.jsx
+++ src/components/headers/MainHeader.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 // Data
 import { mainHeaderImage } from "../../assets";
-import { Link } from "react-router-dom";
 
 export default function MainHeader({ openAssessmentModal }) {
   return (
